5b991c91311f049bab837aa338cdb9686c520553
yohei0511 <yohei0511@65c6b6b4-05fa-11df-864e-45d22c28d447>
2010-01-20 21:35:11 +0000

    とりあえず、直近50件のデータを取得出来る物を作成
    git-svn-id: http://rfinanceyj.googlecode.com/svn/trunk@2 65c6b6b4-05fa-11df-864e-45d22c28d447

---
+++ RFinanceYJ/R/quoteStockTsData.R
@@ -0,0 +1,26 @@
+quoteStockTsData <-
+function( x, since=NULL){
+  stock.data <- data.frame(NULL)
+  quote.url <- paste('http://table.yahoo.co.jp/t?s=', x)
+  try( r <- xmlRoot(htmlTreeParse(quote.url)) )
+  quote.table <- r[[2]][[1]][[1]][[16]][[1]][[1]][[1]][[4]][[1]][[1]][[1]]
+  end <- xmlSize(quote.table)
+
+  for(i in 2:end){
+    tmp <- quote.table[[i]]
+    d <- gsub("^([0-9]{4})([^0-9]+)([0-9]{1,2})([^0-9]+)([0-9]{1,2})([^0-9]+)",
+              "\\1-\\3-\\5",
+              xmlValue(tmp[[1]]))
+    tmp.data <-
+      data.frame(date=as.POSIXct(d),
+                 open=gsub("[^0-9]", "", xmlValue(tmp[[2]])),
+                 height=gsub("[^0-9]", "", xmlValue(tmp[[3]])),
+                 low=gsub("[^0-9]", "", xmlValue(tmp[[4]])),
+                 close=gsub("[^0-9]", "", xmlValue(tmp[[5]])),
+                 volume=gsub("[^0-9]", "", xmlValue(tmp[[6]]))
+                 )
+    stock.data <- rbind(stock.data, tmp.data)
+  }
+  stock.data
+}
+
