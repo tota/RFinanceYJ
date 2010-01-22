bcebed5965f417f3d1ba8f82456a47f90784ad64
yohei0511 <yohei0511@65c6b6b4-05fa-11df-864e-45d22c28d447>
2010-01-22 23:37:05 +0000

    Issue 8 日経平均など出来高がないものも取得出来るように修正
    git-svn-id: http://rfinanceyj.googlecode.com/svn/trunk@8 65c6b6b4-05fa-11df-864e-45d22c28d447

--- RFinanceYJ/R/quoteStockTsData.R
+++ RFinanceYJ/R/quoteStockTsData.R
@@ -11,16 +11,22 @@ function( x, since=NULL){
     d <- gsub("^([0-9]{4})([^0-9]+)([0-9]{1,2})([^0-9]+)([0-9]{1,2})([^0-9]+)",
               "\\1-\\3-\\5",
               iconv(xmlValue(tmp[[1]]), "EUC-JP", "UTF-8", ""))
+    o <- gsub("[^0-9]", "", xmlValue(tmp[[2]]))
+    h <- gsub("[^0-9]", "", xmlValue(tmp[[3]]))
+    l <- gsub("[^0-9]", "", xmlValue(tmp[[4]]))
+    c <- gsub("[^0-9]", "", xmlValue(tmp[[5]]))
+    v <-ifelse(xmlSize(tmp) >= 6, gsub("[^0-9]", "", xmlValue(tmp[[6]])),0)
     tmp.data <-
-      data.frame(date=as.POSIXct(d),
-                 open=gsub("[^0-9]", "", xmlValue(tmp[[2]])),
-                 height=gsub("[^0-9]", "", xmlValue(tmp[[3]])),
-                 low=gsub("[^0-9]", "", xmlValue(tmp[[4]])),
-                 close=gsub("[^0-9]", "", xmlValue(tmp[[5]])),
-                 volume=gsub("[^0-9]", "", xmlValue(tmp[[6]]))
-                 )
+      data.frame(date=d,open=o,height=h,low=l,close=c,volume=v)
     stock.data <- rbind(stock.data, tmp.data)
   }
+  stock.data$date <- as.POSIXct(stock.data$date)
+  stock.data$open <- as.character(stock.data$open)
+  stock.data$height <- as.character(stock.data$height)
+  stock.data$low <- as.character(stock.data$low)
+  stock.data$close <- as.character(stock.data$close)
+  stock.data$volume <- as.character(stock.data$volume)
+  
   stock.data
 }
 
