55f0d9db857c2a8c6e560ad0d6386aa57a7f54bb
yohei0511 <yohei0511@65c6b6b4-05fa-11df-864e-45d22c28d447>
2010-02-03 22:18:52 +0000

    50件以上のデータの取得。sinceの実装
    git-svn-id: http://rfinanceyj.googlecode.com/svn/trunk@20 65c6b6b4-05fa-11df-864e-45d22c28d447

--- RFinanceYJ/R/quoteStockTsData.R
+++ RFinanceYJ/R/quoteStockTsData.R
@@ -1,36 +1,45 @@
 quoteStockTsData <-
 function( x, since=NULL, start.num=0){
+  if( grep("XML", search()) == 0) { library("XML") }
   r <- NULL
+  result.num <- 51
+  quote.table.list <- list(NULL)
   quote.table <- NULL
   stock.data <- data.frame(NULL)
   start <- (gsub("([0-9]{4,4})-([0-9]{2,2})-([0-9]{2,2})","&c=\\1&a=\\2&b=\\3",since))
-  quote.url <- paste('http://table.yahoo.co.jp/t?s=',x,start,'&y=',start.num, sep="")
 
-  try( r <- xmlRoot(htmlTreeParse(quote.url, error=xmlErrorCumulator(immediate=F))) , TRUE)
-  if( is.null(r) ) stop(paste("Can not access :", quote.url))
+  while( result.num == 51 ){
+    quote.url <- paste('http://table.yahoo.co.jp/t?s=',x,start,'&y=',start.num, sep="")
+    try( r <- xmlRoot(htmlTreeParse(quote.url, error=xmlErrorCumulator(immediate=F))) , TRUE)
+    if( is.null(r) ) stop(paste("Can not access :", quote.url))
 
-  try( quote.table <- r[[2]][[1]][[1]][[16]][[1]][[1]][[1]][[4]][[1]][[1]][[1]], TRUE )
-  if( is.null(quote.table) ) stop(paste("Can not quote :", x))
+    try( quote.table <- r[[2]][[1]][[1]][[16]][[1]][[1]][[1]][[4]][[1]][[1]][[1]], TRUE )
+    if( is.null(quote.table) ) stop(paste("Can not quote :", x))
 
-  end <- xmlSize(quote.table)
+    end <- xmlSize(quote.table)
 
-  for(i in 2:end){
-    tmp <- quote.table[[i]]
-    d <- gsub("^([0-9]{4})([^0-9]+)([0-9]{1,2})([^0-9]+)([0-9]{1,2})([^0-9]+)",
-              "\\1-\\3-\\5",
-              iconv(xmlValue(tmp[[1]]), "EUC-JP", "UTF-8", ""))
-    o <- gsub("[^0-9]", "", xmlValue(tmp[[2]]))
-    h <- gsub("[^0-9]", "", xmlValue(tmp[[3]]))
-    l <- gsub("[^0-9]", "", xmlValue(tmp[[4]]))
-    c <- gsub("[^0-9]", "", xmlValue(tmp[[5]]))
-    v <-ifelse(xmlSize(tmp) >= 6, gsub("[^0-9]", "", xmlValue(tmp[[6]])),0)
-    tmp.data <-
-      data.frame(date=as.POSIXct(d),open=o,height=h,low=l,close=c,volume=v)
-    stock.data <- rbind(stock.data, tmp.data)
+    for(i in 2:end){
+      tmp <- quote.table[[i]]
+      d <- gsub("^([0-9]{4})([^0-9]+)([0-9]{1,2})([^0-9]+)([0-9]{1,2})([^0-9]+)",
+                "\\1-\\3-\\5",
+                iconv(xmlValue(tmp[[1]]), "EUC-JP", "UTF-8", ""))
+      o <- gsub("[^0-9]", "", xmlValue(tmp[[2]]))
+      h <- gsub("[^0-9]", "", xmlValue(tmp[[3]]))
+      l <- gsub("[^0-9]", "", xmlValue(tmp[[4]]))
+      c <- gsub("[^0-9]", "", xmlValue(tmp[[5]]))
+      v <-ifelse(xmlSize(tmp) >= 6, gsub("[^0-9]", "", xmlValue(tmp[[6]])),0)
+      tmp.data <-
+        data.frame(date=as.POSIXct(d),open=o,height=h,low=l,close=c,volume=v)
+      stock.data <- rbind(stock.data, tmp.data)
+    }
+    
+    result.num <- xmlSize(quote.table)
+    start.num <- start.num + 50
+    Sys.sleep(1)
   }
-
+  
   stock.data <- stock.data[order(stock.data$date),]
   stock.data <- data.frame(date=stock.data$date, lapply(stock.data[,-1], function(x){ as.double(as.character(x))}))
-  stock.data
+  stock.data  
 }
 
